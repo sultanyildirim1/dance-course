dcf8b3de71cc05810542137549a3f1194e5fdad2
sultanyildirim1 <147000413+sultanyildirim1@users.noreply.github.com>
2024-06-03 00:11:11 +0300

    Add files via upload

---
+++ dance-course/script.js
@@ -0,0 +1,11 @@
+//document.addEventListener("DOMContentLoaded", function() {
+
+   // var button = document.getElementById("myButton");
+    
+   
+  //  button.addEventListener("click", function() {
+       
+    //    var message = document.getElementById("message");
+        
+       
+      //  message.textContent = "Butona tıkladınız!";
